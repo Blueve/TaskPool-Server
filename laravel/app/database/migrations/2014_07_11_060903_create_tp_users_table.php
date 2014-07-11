d87a2759a84435d338fe68ff80d643a94aa8a081
Blueve <672454911@qq.com>
2014-07-11 16:05:16 +0800

    创建Task Pool Laravel框架

---
+++ laravel/app/database/migrations/2014_07_11_060903_create_tp_users_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTpUsersTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		// 创建tp_users表
+		Schema::create('tp_users', function($table)
+		{
+			$table->increments('id');						// 用户id
+			$table->string('name', 16)->unique();			// 用户名
+			$table->string('email')->unique();				// 用户email
+			$table->char('psw_hash', 32);					// 经过Hash后的密码
+			$table->char('psw_salt', 8);					// Hash算法的Salt
+			$table->dateTime('create_at');					// 用户创建时间
+			$table->boolen('confirmed')->default(false);	// 邮箱验证状态
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('tp_users');
+	}
+
+}
---
+++ laravel/app/database/migrations/2014_07_11_075104_create_tp_tobeconfirmed_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTpTobeconfirmedTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		// 创建tp_tobeconfirmed表
+		Schema::create('tp_tobeconfirmed', function($table)
+		{
+			$table->integer('user_id')				// 用户id
+			$table->char('check_code', 64);			// 校验码(页面标识串)
+			$table->timestamp('created_at');		// 创建时间戳
+			$table->enum('type', 
+				array('findpsw', 'signin'));		// 确认类型
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('tp_tobeconfirmed');
+	}
+
+}
