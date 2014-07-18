c3926672886afb92a31935fdc130286405fccb61
EchoYang33 <echo_register@163.com>
2014-07-18 15:12:38 +0800

    增加edit视图

---
+++ laravel/app/views/user/edit.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.base')
+
+@section('content')
+  <div class="col-md-4 col-md-offset-4 column">
+    {{-- 编辑个人信息 --}}
+    <form role="form" method="post" id="my_edit_form" action="{{{ URL::action('UserController@edit') }}}">
+      <div class="form-group">
+         <label for="oldPassword">原密码</label><input type="password" class="form-control" id="oldPassword" name="oldPassword" />
+      </div>
+      <div class="form-group">
+         <label for="password">新密码</label><input type="password" class="form-control" id="password" name="password" />
+      </div>
+      <div class="form-group">
+         <label for="passwordConfirm">新密码</label><input type="password" class="form-control" id="passwordConfirm" name="passwordConfirm" />
+      </div>
+      <div class="form-group">
+        <button type="submit" class="btn btn-lg btn-primary btn-block">提交更改</button>
+      </div>
+    </form>
+  </div>
+@stop
