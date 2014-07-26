d19d928a1301ecb02adbae8ef4ef4977e17c4e8e
Blueve <672454911@qq.com>
2014-07-27 01:21:34 +0800

    增加了UserTable的Seeder

--- laravel/app/database/seeds/DatabaseSeeder.php
+++ laravel/app/database/seeds/DatabaseSeeder.php
@@ -11,7 +11,24 @@ class DatabaseSeeder extends Seeder {
 	{
 		Eloquent::unguard();
 
-		// $this->call('UserTableSeeder');
+		$this->call('UserTableSeeder');
+		$this->command->info('User table seeded!');
+
 	}
 
+}
+
+class UserTableSeeder extends Seeder {
+
+	public function run()
+	{
+		// 产生一个通过验证的账号
+		$user = new User;
+		$user->name = 'tester';
+		$user->email = 'tester@example.com';
+		list($user->psw_hash, $user->psw_salt) = Helper::HashPassword('123456789');
+		$user->created_at = date('Y-m-d H:i:s', time());
+		$user->confirmed = true;
+		$user->save();
+	}
 }
