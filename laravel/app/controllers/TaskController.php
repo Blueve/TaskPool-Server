8580564ec6a870c2a0886ac05fe478c112b210ff
Blueve <672454911@qq.com>
2014-08-05 14:36:37 +0800

    增加了Task创建的控制器模板及模型

--- laravel/app/controllers/TaskController.php
+++ laravel/app/controllers/TaskController.php
@@ -2,5 +2,14 @@
 
 class TaskController extends BaseController {
 
+	public function create()
+	{
+		$user = Auth::user();
+		
+		$tasks = Task::newTask($newTaskForm, $user);
+
+		
+		return Response::json($response);
+	}
 
 }
