7abe890e332b6c45c54ed001757acdea4e1fef10
Blueve <672454911@qq.com>
2014-08-06 11:01:05 +0800

    增加FontAwesome插件

--- laravel/app/views/layouts/base.blade.php
+++ laravel/app/views/layouts/base.blade.php
@@ -9,6 +9,7 @@
     <!-- Bootstrap -->
     <link href="{{{ asset('css/bootstrap.min.css') }}}" rel="stylesheet">
     <link href="{{{ asset('css/icheck/all.css') }}}" rel="stylesheet">
+    <link href="{{{ asset('css/font-awesome.min.css') }}}" rel="stylesheet">
     <link href="{{{ asset('css/common.css') }}}" rel="stylesheet">
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
