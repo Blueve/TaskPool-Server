87a47bb2974e30892716cfd8988afe090a849bf2
Blueve <672454911@qq.com>
2014-08-11 17:19:59 +0800

    使用了新的配置路径

--- src/app/views/emails/welcome.blade.php
+++ src/app/views/emails/welcome.blade.php
@@ -8,8 +8,8 @@
 
 		<div>
 			为了完成注册过程，您需要访问下面的地址来验证你的邮箱：<br/>
-			{{ URL::to('user/confirm', array($userId, $checkCode)) }}<br/>
-			请在{{{ Config::get('app.confirm_limit') / 60 }}}分钟内完成确认
+			{{ URL::to('confirm', array($userId, $checkCode)) }}<br/>
+			请在{{{ Config::get('site.confirm_limit') / 60 }}}分钟内完成确认
 		</div>
 	</body>
 </html>
