6ca085d837fdd399649bd739c526e05b9d4d741f
Blueve <672454911@qq.com>
2014-08-20 15:26:17 +0800

    调整数据表结构
    出于用户可以拥有其他用户的列表，并可以拥有自己的顺序，将用户拥有的List单独建表

--- src/app/database/migrations/2014_07_22_024153_create_tp_lists_table.php
+++ src/app/database/migrations/2014_07_22_024153_create_tp_lists_table.php
@@ -31,25 +31,30 @@ class CreateTpListsTable extends Migration {
 					'darkgray',
 					'gray'))->default('darkgray');
 			$table->enum('icon', Config::get('iconset.list_icon_set'))
-				->default(Config::get('iconset.default_icon'));
-			$table->integer('priority')->unsigned();	// 列表顺位
-			$table->enum('sort_by', 					// 内部排序方式
+				  ->default(Config::get('iconset.default_icon'));	// 列表图标
+			$table->enum('sort_by', 								// 内部排序方式
 				array(
 					'important', 
 					'urgent', 
 					'date',
 					'custom'))->default('important');
+			$table->boolean('shareable')->default(false);	// 是否可共享
+
 			$table->timestamps();						// created_at update_at 时间戳
 			$table->softDeletes();						// deleted_at	
 			$table->integer('version')->unsigned();		// 版本号
 		});
 
-		// tp_sharedlist
-		Schema::create('tp_sharedlists', function($table)
+		// tp_userlist
+		Schema::create('tp_userlist', function($table)
 		{
-			$table->increments('id');					// id
-			$table->integer('list_id')->unsigned();		// 可共享列表id
-			$table->char('code', 10)->unique();			// 共享码
+			$table->increments('id');					// 用户列表id
+			$table->integer('user_id')->unsigned();		// 用户id
+			$table->integer('list_id')->unsigned();		// 列表id
+			$table->integer('priority')->unsigned();	// 列表顺位
+			$table->timestamps();						// created_at update_at 时间戳
+			$table->softDeletes();						// deleted_at
+			$table->integer('version')->unsigned();		// 版本号
 		});
 
 		// tp_grp_list
@@ -68,7 +73,7 @@ class CreateTpListsTable extends Migration {
 	public function down()
 	{
 		Schema::drop('tp_grp_list');
-		Schema::drop('tp_sharedlists');
+		Schema::drop('tp_userlist');
 		Schema::drop('tp_lists');
 	}
 
