d13e670ae32021dfce76f8d4c33c6538688716c5
Blueve <672454911@qq.com>
2014-08-21 13:58:52 +0800

    更新了DatabaseSeeder
    增加了默认列表以及另外一个用户

--- src/app/database/seeds/DatabaseSeeder.php
+++ src/app/database/seeds/DatabaseSeeder.php
@@ -1,7 +1,7 @@
 <?php
 
-class DatabaseSeeder extends Seeder {
-
+class DatabaseSeeder extends Seeder 
+{
 	/**
 	 * Run the database seeds.
 	 *
@@ -11,15 +11,15 @@ class DatabaseSeeder extends Seeder {
 	{
 		Eloquent::unguard();
 
-		$this->call('UserTableSeeder');
-		$this->command->info('User table seeded!');
+		$this->call('TestSeeder');
+		$this->command->info('Test seeded!');
 
 	}
 
 }
 
-class UserTableSeeder extends Seeder {
-
+class TestSeeder extends Seeder 
+{
 	public function run()
 	{
 		// 产生一个通过验证的账号
@@ -30,5 +30,27 @@ class UserTableSeeder extends Seeder {
 		$user->created_at = date('Y-m-d H:i:s', time());
 		$user->confirmed = true;
 		$user->save();
+		// 产生另一个通过验证的账号
+		$user2 = new User;
+		$user2->name = 'tester2';
+		$user2->email = 'tester2@example.com';
+		list($user2->psw_hash, $user2->psw_salt) = Helper::HashPassword('123456789');
+		$user2->created_at = date('Y-m-d H:i:s', time());
+		$user2->confirmed = true;
+		$user2->save();
+		// 给$user添加默认的List
+		$listArr = ['工作', '学习', '旅行', '书单', '愿望'];
+		foreach ($listArr as $value)
+		{
+			$listForm = new NewListForm(['name' => $value]);
+			TaskList::createByForm($listForm, $user);
+		}
+		// 给$user2添加默认的List
+		$listArr = ['共享1', '共享2', '指派1', '指派2'];
+		foreach ($listArr as $value)
+		{
+			$listForm = new NewListForm(['name' => $value]);
+			TaskList::createByForm($listForm, $user2);
+		}
 	}
 }
