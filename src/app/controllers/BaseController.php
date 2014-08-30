7ba516f9bbaa7dcd72eab238447e6b1cb7dd1d79
Blueve <672454911@qq.com>
2014-08-30 13:13:48 +0800

    为代码添加文档型注释

--- src/app/controllers/BaseController.php
+++ src/app/controllers/BaseController.php
@@ -19,16 +19,41 @@ class BaseController extends Controller
 		 );
 	}
 
+	/**
+	 * 将指定的数据并入视图数据集中
+	 * 
+	 * @param  array $arr 数据集
+	 * @return void
+	 */
 	protected function MergeData($arr)
 	{
 		$this->data = array_merge($this->data, $arr);
 	}
 
+	/**
+	 * 设置页面的Tag
+	 *
+	 * 设置页面Tag后，在视图中，若与指定Tag匹配，则会按照视图
+	 * 的设定增加额外的显示效果，例如导航栏选中的效果
+	 * 
+	 * @param  string $tagName Tag
+	 * @return void
+	 */
 	protected function SetPageTag($tagName)
 	{
 		$this->data['pageTag'] = $tagName;
 	}
 
+	/**
+	 * 返回一个提示页面
+	 * 
+	 * @param  string $pageLang   页面基本信息对应的语言包空间
+	 * @param  string $type       提示类型：success\danger\warning\info
+	 * @param  string $notice     提示内容对应的语言包空间
+	 * @param  string $route      跳转目标Route
+	 * @param  array  $routeValue 跳转目标Route附加串
+	 * @return View               提示页面
+	 */
 	protected function NoticeResponse($pageLang, $type, $notice, $route = '/', $routeValue = array())
 	{
 		$notice = new Notice($type, $notice, $route, $routeValue);
--- src/app/libraries/Helper.php
+++ src/app/libraries/Helper.php
@@ -2,38 +2,83 @@
 
 class Helper
 {
-	static public function VarDump($var)
+	/**
+	 * 在页面上输出指定变量
+	 * 
+	 * @param  Object $var 需要被输出的变量
+	 * @return void
+	 */
+	public static function VarDump($var)
 	{
 		echo '<pre>';
 		print_r($var);
 		echo '</pre>';
 	}
 
-	static public function HashPassword($password)
+	/**
+	 * 按照MD5 + Salt的方法对指定明文密码加密
+	 * 
+	 * @param  string $password 明文密码
+	 * @return array            [0]=>密码哈希值,[1]=>密码Salt
+	 */
+	public static function HashPassword($password)
 	{
 		$psw_salt = str_random(8);
 		$psw_hash = md5($password.$psw_salt);
 		return array($psw_hash, $psw_salt);
 	}
 
-	static public function CheckPassword($psw_hash, $psw_salt, $password)
+	/**
+	 * 按照MD5 + Salt的方法对指定明文密码和哈希密码进行校验
+	 * 
+	 * @param  string $psw_hash 密码哈希值
+	 * @param  string $psw_salt 密码Salt
+	 * @param  string $password 密码明文
+	 * @return boolean          是否匹配
+	 */
+	public static function CheckPassword($psw_hash, $psw_salt, $password)
 	{
 		return $psw_hash === md5($password.$psw_salt);
 	}
 
-	static public function isExpired($date, $limit)
+	/**
+	 * 判断是否过期
+	 *
+	 * 根据时间限制和起始时间进行判断，若当前时间距离起始时间
+	 * 已经超过了时间限制，那么则判定为过期
+	 * 
+	 * @param  int $date  起始时间戳
+	 * @param  int $limit 时间限制（秒）
+	 * @return boolean    是否过期
+	 */
+	public static function IsExpired($date, $limit)
 	{
 		return (time() - strtotime($date)) < $limit ? false : true ;
 	}
 
-	static public function EncodeListId($id)
+	/**
+	 * 以Base64算法对ListId进行编码
+	 *
+	 * 将待编码的ListId进行补零操作后再进行编码，从而确保产生
+	 * 的编码不出现特殊符号
+	 * 
+	 * @param  int $id 列表Id
+	 * @return string  编码后的列表Id
+	 */
+	public static function EncodeListId($id)
 	{
 		$len = strlen($id);
 		$len = $len + (3 - $len % 3);
 		return base64_encode(str_pad($id, $len, '0', STR_PAD_LEFT));
 	}
 
-	static public function DecodeListId($code)
+	/**
+	 * 以Base64算法对ListId进行解码
+	 * 
+	 * @param  string $code 编码后的列表Id
+	 * @return int          解码后的列表Id
+	 */
+	public static function DecodeListId($code)
 	{
 		return intval(base64_decode($code));
 	}
