cb90fe174da53adda675042d93c8c6543ae7a047
Blueve <672454911@qq.com>
2014-08-31 12:04:47 +0800

    代码规范化

--- src/app/controllers/HomeController.php
+++ src/app/controllers/HomeController.php
@@ -1,7 +1,14 @@
 <?php
 
-class HomeController extends BaseController {
-
+class HomeController extends BaseController
+{
+	/**
+	 * 首页
+	 *
+	 * 包含了注册/登录界面，可以引导用户进行找回密码等操作
+	 * 
+	 * @return View 页面
+	 */
 	public function startup()
 	{
 		$this->MergeData(Lang::get('base.startup'));
