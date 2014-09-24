e3acb732ce71fb681cd129f0887047179b5d8f20
Blueve <672454911@qq.com>
2014-09-24 18:11:07 +0800

    更新创建任务表单的语言条目

--- src/app/lang/cn/task.php
+++ src/app/lang/cn/task.php
@@ -19,18 +19,29 @@ return array(
 	"urgent"            => "紧急的任务优先",
 	"date"              => "按照任务开始时间",
 	"custom"            => "自定义顺序",
-
-	"create_list"		=> "创建新列表",
-
+	
+	"create_list"       => "创建新列表",
+	
 	"list_color"        => "列表颜色",
 	"list_icon"         => "列表图标",
-
-	"shareable"			=> "共享此列表",
-
+	
+	"shareable"         => "共享此列表",
+	
 	"share_code"        => "共享码",
-
-	"create_task"       => "快捷创建任务",
-
+	
+	"create_task_quick" => "快捷创建任务",
+	"create_task"       => "创建新任务",
+	
 	"create_task_more"  => "显示更多选项",
 	"create_task_less"  => "恢复快捷创建选项",
+	
+	"description"       => "任务详情",
+	
+	"start"             => "开始于",
+	"end"               => "结束于",
+	
+	"i_u"               => "重要且紧急",
+	"i_nu"              => "重要但不紧急",
+	"ni_u"              => "紧急但不重要",
+	"ni_nu"             => "不重要也不紧急",
 );
